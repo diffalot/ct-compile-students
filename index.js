d3dd9039e9d2a1c3cab8eb7619552acbe697fc81
Alice Davis <alice@gigantic.computer>
2020-07-02 14:52:07 -0700

    init

---
+++ index.js
@@ -0,0 +1,92 @@
+// Reads csv exports from the `data/students.csv` and `data/attendance.csv` spreadsheets and generates a unified, deduplicated list of all students in `data/all-students.csv`
+const {
+    readFile,
+    writeFile
+} = require('fs').promises
+const {
+    join
+} = require('path')
+const parse = require('csv-parse/lib/sync');
+
+const readCSV = async (path /* TODO: needs a better name or to be an already normalized path */ ) => {
+    const fileLocation = join(__dirname, path)
+    const file = await readFile(fileLocation)
+    return parse(file, {
+        columns: true
+    })
+}
+
+const main = async function () {
+    console.log('Running Main')
+    const allStudents = new Map()
+    const students = await readCSV('data/students.csv')
+    const attendance = await readCSV('data/attendance.csv')
+
+    // find student by email address, so no multiples exist upon setting a student, should become a method used when inserting students from the attendance array
+    const findStudentsByEmail = (email) => {
+        if (email.length === 0) return []
+        const foundStudents = []
+        allStudents.forEach((student) => {
+            if (student.emailAddresses.indexOf(email.toLowerCase()) >= 0) {
+                foundStudents.push(student)
+            }
+        })
+        return foundStudents
+    }
+
+    debugger
+
+    // loop through students and add them to the map first because students is the authoratiative source
+    students.forEach((student) => {
+        let emailAddress = student['Emails'].toLowerCase()
+        let emailAddresses = [emailAddress]
+
+        if (emailAddress.indexOf(',') >= 0) {
+            // student has multiple email addresses
+
+            emailAddresses = emailAddress
+                .split(',')
+                .map(s => s.trim().toLowerCase())
+
+            // use first email listed as the key
+            emailAddress = emailAddresses[0]
+        }
+
+
+        // TODO: only set student if there is no other email address for this student in `allStudents`.  Halt and error if key exists
+        if (emailAddress.length > 0 && student.Notes.indexOf('NEEDS INVESTIGATION') < 0) {
+            if (allStudents.get(emailAddress)) {
+                debugger
+                throw (new Error(`${emailAddress} already exists: \n ${JSON.stringify(allStudents.get(emailAddress))}`))
+            }
+            allStudents.set(emailAddress, {
+                ...student,
+                emailAddresses
+            })
+        }
+    })
+
+
+    // TODO: Insert students found in the `attendance.csv`
+    const missingStudents = []
+    attendance.forEach((logEntry) => {
+       
+        const foundStudents = findStudentsByEmail(logEntry['Email Address'])
+
+        if (foundStudents.length === 0 && logEntry['Email Address'].length > 0) {
+            if (logEntry['Email Address'] === 'reneektom@gmail.com') debugger
+            missingStudents.push(logEntry)
+        }
+    }) 
+
+    missingStudents.forEach((logEntry) => {
+        console.log(`could not find ${logEntry['Full Name']} as ${logEntry['Email Address']} in ${logEntry['Are you a member of the Morning or Afternoon Class']} class`)
+    })
+
+    if (missingStudents.length > 0) throw new Error(`found ${missingStudents.length} missing students`)
+
+    debugger
+}
+
+// run it!
+main()
